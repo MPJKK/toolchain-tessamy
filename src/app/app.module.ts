ef946291b9ef05bf25b65d32e5adcf4e944420ea
Tessa Myllys <tessam@ety-a2123b-15.edu.wks.metropolia.fi>
2018-01-16 13:11:50 +0200

    Tehtävä valmis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
